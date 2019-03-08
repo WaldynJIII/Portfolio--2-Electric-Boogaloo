a0af1c042cce761da1f36c8295522d7eb08020d0
Waldyn Benbenek <wben1201@gmail.com>
2019-03-08 14:38:06 -0600

    route to server set up.

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
-
+const portfolioRouter = require('./routes/portfolio.router.js');
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
-
+app.use('/api/portfolio', portfolioRouter)
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
