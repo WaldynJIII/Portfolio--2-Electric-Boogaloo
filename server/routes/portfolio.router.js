80acc6649992178098a257e95b1974f2da6c65bf
Waldyn Benbenek <wben1201@gmail.com>
2019-03-10 12:35:27 -0500

    forgot to to do last commit on Friday. Still not great,

---
+++ server/routes/portfolio.router.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const pool = require('../modules/pool');
+
+const router = express.Router()
+router.get('/', (req, res) => {
+
+
+
+    const queryText = `SELECT * FROM "projects"  ORDER BY "id" ASC`;
+
+    pool.query(queryText)
+        .then((result) => {
+            res.send(result.rows);
+            console.log(result.rows)
+
+        })
+        .catch((error) => {
+            console.log(`Error on query ${error}`);
+            res.sendStatus(500);
+        });
+
+});
+module.exports = router;
